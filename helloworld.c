df47cba49a4c37b818937c37b3992a540db6edd5
sawera <241689067+echomesawera@users.noreply.github.com>
2026-02-21 20:38:30 +0500

    moved my files to programing

--- helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int add(){}
-
-int main(void)
-{
-printf ("Programming is fun.\n");
-return 0;
-}
--- programming/first.c
+++ programming/first.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-int main(){
-printf("Hello World!!");
-return 0
+int main(){ 
+    int n;
+
+    for(int i = 1; i <= 20; i++) {
+        printf("Enter your number: ");
+        scanf("%d", &n);
+
+        if(n % 2 != 0) {  // Agar number odd hai
+            printf("Odd number mila, loop stop!\n");
+            break;  // Loop yahan stop ho jayega
+        }
+
+        printf("Even number, loop continue...\n");
+    }
+
+    printf("End\n");
+
+return 0;
 }
