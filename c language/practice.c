71319e360b40ed453a3576e3710a59a13ceccb3b
sawera <241689067+echomesawera@users.noreply.github.com>
2026-03-19 19:32:43 +0500

    My project

---
+++ c language/practice.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+
+int r ;
+printf("enter your number of row");
+scanf("%d",&r);
+
+int c ;
+printf("enter your number of colum");
+scanf("%d",&c);
+
+int arr[r][c];
+
+for(int i = 0;i < r;i++){
+
+for(int j =0;j < c;j++){
+
+scanf("%d",&arr[i][j]);
+
+}
+}
+
+for(int i = 0; i < c;i++){
+
+for(int j = 0; j < r;j++){
+printf("%d",arr[j][i]);
+
+}
+printf("\n");
+}
+
+return 0;    
+}
